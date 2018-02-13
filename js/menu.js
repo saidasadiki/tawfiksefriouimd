a412722e18deb8b3e5ee0e17e44bcc0f8d6639b6
saidasadiki <saida.sadiki123@gmail.com>
2018-02-13 19:40:11 +0000

    update menu

--- js/menu.js
+++ js/menu.js
@@ -8,10 +8,8 @@ l'argument passé à la fonction append doit être dans une seule ligne.
 
 .*/ 
 
-<<<<<<< HEAD
-$("#menu").append('<li ><a href="index.html"><span>Home</span></a> </li> <li ><a href="profil.html"><span>Profil</span></a> </li> <li><a><span>Services</span></a><ul class="rd-navbar-dropdown"> <li><a href="women.html"><span>Cosmetic surgery for women</span></a> </li>  <li><a href="medspa.html"><span class="text-middle">Non-surgical procedure / med Spa</span></a> </li>   <li><a href="men.html"><span class="text-middle">Cosmetic surgery For Men</span></a> </li>  <li><a href="Genital surgery.html"><span class="text-middle">Genital surgery</span></a> </li> <li><a href="Regenerativemedicine.html"><span class="text-middle">Regenerative medicine</span></a> </li> </ul><li><a href="gallery.html"><span>Gallery</span></a> </li> <li><a href="training.html"><span>Training</span></a> </li> <li><a href="contact.html"><span>Contact</span></a> </li>');
+
+$("#menu").append('<li ><a href="index.html"><span>Home</span></a> </li> <li ><a href="profil.html"><span>Profil</span></a> </li> <li><a><span>Services</span></a><ul class="rd-navbar-dropdown"> <li><a href="women.html"><span>Cosmetic surgery for women</span></a> </li>  <li><a href="medspa.html"><span class="text-middle">Non-surgical procedure / med Spa</span></a> </li>   <li><a href="men.html"><span class="text-middle">Cosmetic surgery For Men</span></a> </li>  <li><a href="Genital surgery.html"><span class="text-middle">Genital surgery</span></a> </li> <li><a href="regenerativemedicine.html"><span class="text-middle">Regenerative medicine</span></a> </li> </ul><li><a href="gallery.html"><span>Gallery</span></a> </li> <li><a href="training.html"><span>Training</span></a> </li> <li><a href="contact.html"><span>Contact</span></a> </li>');
 $("#menu li:contains('"+ $("#menu").attr("active") +"')").addClass("active");
-=======
-jQuery("#menu").append('<li ><a href="index.html"><span>Home</span></a> </li> <li ><a href="profil.html"><span>Profil</span></a> </li> <li><a><span>Services</span></a><ul class="rd-navbar-dropdown"> <li><a href="women.html"><span class="text-middle">Cosmetic surgery for women</span></a> </li>  <li><a href="medspa.html"><span class="text-middle">Non-surgical procedure / med Spa</span></a> </li>   <li><a href="men.html"><span class="text-middle">Cosmetic surgery For Men</span></a> </li>  <li><a href="Genital surgery.html"><span class="text-middle">Genital surgery</span></a> </li> <li><a href="Regenerativemedicine.html"><span class="text-middle">Regenerative medicine</span></a> </li> </ul><li><a href="gallery.html"><span>Gallery</span></a> </li> <li><a href="training.html"><span>Training</span></a> </li> <li><a href="contact.html"><span>Contact</span></a> </li>');
-jQuery("#menu li:contains('"+ jQuery("#menu").attr("active") +"')").addClass("active");
->>>>>>> 580c0e3e8b4064cc7933c1adda4bcd1444f95f81
+
+
